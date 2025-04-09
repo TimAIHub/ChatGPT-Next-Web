1deb4c66870ddae028be867db389ac201a160418
TimAIHub <lm.chatgpt@gmail.com>
2025-04-09 12:48:19 +0800

    Update layout.tsx, fix crossOrigin

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({
         <script 
           async 
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6251937866531497"
-          crossorigin="anonymous">
+          crossOrigin="anonymous">
         </script>
         <AdSense pId="6251937866531497" />
       </head>
